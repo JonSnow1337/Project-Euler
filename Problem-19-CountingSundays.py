e2b105a7e0710bcef62f436c1f68edac1be39c86
Jakov Peric <zgblok@gmail.com>
2015-07-25 18:06:28 +0200

    Problem 19 solved

--- Problem-19-CountingSundays.py
+++ Problem-19-CountingSundays.py
@@ -8,16 +8,24 @@ All the rest have thirty-one,
 Saving February alone,
 Which has twenty-eight, rain or shine.
 And on leap years, twenty-nine.
-A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+A leap year occurs on any year evenly divisible by 4,
+but not on a century unless it is divisible by 400.
 
-How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+How many Sundays fell on the first of the month during 
+the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+
+How I solved it:
+    -made a function that knows name of day by inputing date
+    -it calculates number of days from 1.1.1900 to input date
+    -when days have been calculated by doing mod 7 I know name of day
+
+    -Thats the hard part, then we just go from all  first days in century,
+     see if its a sunday and count up
 """
-""" -have function know what name of day was for specific date (input day, month, year)
-    - build all dates from 1 jan 1901 to 31 dec 2000
-    - for each of first months ask if it is sunday
-"""
+
 monthDays =[None,31,28,31,30,31,30,31,31,30,31,30,31]
 dayNames = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+
 def isLeapYear(year):
     if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
         return True
@@ -54,7 +62,15 @@ def getDateName(day, month, year):
         daysBetweenMonths += numberOfDaysInMonth(m, year) 
 
     daysFromPivotDate = day + daysBetweenYears + daysBetweenMonths
-    print dayNames[daysFromPivotDate % 7]
+    return dayNames[daysFromPivotDate % 7]
 
+#problem solverino.
+sundays = 0
+for year in range(1901, 2001):
+    for month in range(1,13):
+        if getDateName(1,month,year) == "sunday":
+            sundays += 1
+
+print sundays
 
 
